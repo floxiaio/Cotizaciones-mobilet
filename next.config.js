843ba82bb7dc377b9aa7a93764f56f511341f76f
floxiaio <riosje@gmail.com>
2025-07-12 22:13:08 -0600

    Fix: Remove backup file and update configs

--- next.config.js
+++ next.config.js
@@ -1,17 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
   images: {
     domains: ['localhost'],
   },
   webpack: (config) => {
     config.resolve.alias['@'] = __dirname + '/src';
     return config;
-  },
-  experimental: {
-    appDir: true,
-  },
+  }
 };
 
 module.exports = nextConfig;
